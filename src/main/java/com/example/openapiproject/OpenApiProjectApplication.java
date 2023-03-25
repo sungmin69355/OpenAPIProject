ff5f2e90ce29cec284da0bc1f1388c0cb3c21995
sungmin69355 <sungmin69355@naver.com>
2023-03-25 22:05:42 +0900

    first commit

---
+++ src/main/java/com/example/openapiproject/OpenApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.openapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiProjectApplication.class, args);
+    }
+
+}
